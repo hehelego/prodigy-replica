92e3cfde41534715a523b259760db8f5fec1d3c1
hehelego <hahalegao@gmail.com>
2024-05-29 15:16:23 +0800

    fix test data

--- impl/test.fish
+++ impl/test.fish
@@ -7,17 +7,14 @@ function check
     echo $q | python main.py $p $q
 end
 
-check geometric_shifted.pgcl geometric_shifted_invariant.pgcl
-exit
-
 check dep_bern.pgcl dep_bern_invariant.pgcl
-check endless_conditioning.pgcl endless_conditioning_invariant.pgcl
 check dueling_cowboys_parameter.pgcl dueling_cowboys_parameter_invariant.pgcl
 
-check geometric.pgcl geometric_invariant.pgcl check geometric_observe.pgcl geometric_observe_invariant.pgcl check geometric_observe_parameter.pgcl geometric_observe_parameter_invariant.pgcl
+check geometric.pgcl geometric_invariant.pgcl
+check geometric_observe.pgcl geometric_observe_invariant.pgcl
+check geometric_observe_parameter.pgcl geometric_observe_parameter_invariant.pgcl
 check geometric_parameter.pgcl geometric_parameter_invariant.pgcl
 check geometric_shifted.pgcl geometric_shifted_invariant.pgcl
-
 
 check ky_die.pgcl ky_die_invariant.pgcl
 check ky_die_2.pgcl ky_die_2_invariant.pgcl
@@ -32,6 +29,7 @@ check random_walk.pgcl random_walk_invariant.pgcl
 check running_paper_example.pgcl running_paper_example_invariant.pgcl
 
 check trivial_iid.pgcl trivial_iid_invariant.pgcl
+
 
 # test scalability
 echo "Scalability benchmarking:"
